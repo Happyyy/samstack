4119103a4ad483634da208c6d5f0df2a93c347dc
Huanhuan Feng <huanhuan.feng@analog.com>
2019-04-09 16:06:10 +0800

    First version of deployed sam stack web server
    Signed-off-by: Huanhuan Feng <huanhuan.feng@analog.com>

---
+++ precache-manifest.688e178ba6a9a173046c5d155886c4cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "01a6585159422966661e",
+    "url": "/static/css/main.0ce8044b.chunk.css"
+  },
+  {
+    "revision": "01a6585159422966661e",
+    "url": "/static/js/main.34fa2704.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b349df7f7f44637924f8",
+    "url": "/static/css/2.fbe3af13.chunk.css"
+  },
+  {
+    "revision": "b349df7f7f44637924f8",
+    "url": "/static/js/2.046341bf.chunk.js"
+  },
+  {
+    "revision": "7e10d02d837fa61914181990942c08fb",
+    "url": "/index.html"
+  }
+];
