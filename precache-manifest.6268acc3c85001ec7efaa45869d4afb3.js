8d6b0f041b480f3a8cdd13a5255eab1130cfb744
Huanhuan Feng <huanhuan.feng@analog.com>
2019-04-09 16:18:27 +0800

    update URL
    Signed-off-by: Huanhuan Feng <huanhuan.feng@analog.com>

---
+++ precache-manifest.6268acc3c85001ec7efaa45869d4afb3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7e97cf3466001c1fa590",
+    "url": "/users/hfeng/repo/samstack-build/static/css/main.0ce8044b.chunk.css"
+  },
+  {
+    "revision": "7e97cf3466001c1fa590",
+    "url": "/users/hfeng/repo/samstack-build/static/js/main.60937f9b.chunk.js"
+  },
+  {
+    "revision": "fccf5f971fd6ba27f431",
+    "url": "/users/hfeng/repo/samstack-build/static/js/runtime~main.31e7a433.js"
+  },
+  {
+    "revision": "b349df7f7f44637924f8",
+    "url": "/users/hfeng/repo/samstack-build/static/css/2.fbe3af13.chunk.css"
+  },
+  {
+    "revision": "b349df7f7f44637924f8",
+    "url": "/users/hfeng/repo/samstack-build/static/js/2.046341bf.chunk.js"
+  },
+  {
+    "revision": "d6f2dc9a56887fc0b9b6eb74b26e86be",
+    "url": "/users/hfeng/repo/samstack-build/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.688e178ba6a9a173046c5d155886c4cb.js"
+  "/users/hfeng/repo/samstack-build/precache-manifest.6268acc3c85001ec7efaa45869d4afb3.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/users/hfeng/repo/samstack-build/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
